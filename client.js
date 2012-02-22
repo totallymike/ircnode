d2d4505cd09e20c7c6f684b9c355187048482054
Michael Westbom <totallymike@gmail.com>
2012-02-22 09:56:29 -0500

    Added some things, cleaned up some style.

--- client.js
+++ client.js
@@ -13,19 +13,30 @@ var plugin_dir  = config_path + '/plugins/';
 irc.config = JSON.parse(fs.readFileSync(config_file));
 
 fs.readFile(user_file, function(err, data) {
+  if (err) {
+    console.log("users.json file must exist!");
+    throw err
+  }
   irc.users = JSON.parse(data);
 });
 
 irc.command_char = '!';
-irc.debug = false;
+irc.debug = true;
 irc.emitter = new events.EventEmitter();
 
 irc.is_admin = function(nick) {
   if (typeof irc.users[nick] === 'undefined')
     return false;
   
-  return (irc.users[nick].auth === 'admin');
+  return (irc.users[nick].auth === 'admin' || irc.users[nick].auth === 'owner');
 }
+
+irc.is_owner = function(nick) {
+  if (typeof irc.users[nick] === 'undefined')
+    return false;
+
+  return (irc.users[nick].auth === 'owner');
+};
 
 irc.privmsg  = function (chan, msg) {
   irc._socket.write('PRIVMSG ' + chan + ' :' + msg + '\r\n');
@@ -60,45 +71,47 @@ irc.act = function(options, callback) {
   if (typeof options.longParam !== 'undefined') {
     string += ' :' + options.longParam;
   }
-  if (typeof callback !== 'function') {
-    callback = null;
-  }
   if (irc.debug) console.log(string);
   irc._socket.write(string + '\r\n', callback);
 };
+
+irc.join = function (channel, callback) {
+  irc.act({action: 'JOIN', params: [channel]}, callback);
+}
 
 irc.nick = function (nick, callback) {
   irc.act({action: 'NICK', params: [nick]}, callback);
 };
 
-var getNewAdmin = function(act) {
-  if (typeof irc.users[act.nick] === 'undefined') {
-    irc.users[act.nick] = {};
-  }
-  irc.users[act.nick].auth = 'admin';
-  irc.act({action: 'PART', params: ['#bot_needs_admin']});
-  irc.emitter.removeListener('hello', getNewAdmin);
-  irc.act({action: 'JOIN', params: [irc.config.chan]});
-}
+irc.user = function (user, mode, realName, callback) {
+  irc.act({action: 'USER', params: [user, mode, '*'],
+          longParam: realName}, callback);
+};
 
 irc._socket = net.connect(irc.config.port, irc.config.address, function () {
-  var nick = irc.config.nick;
-  var user = irc.config.user;
-  var realName = irc.config.realName;
+  var nick      = irc.config.nick;
+  var user      = irc.config.user;
+  var realName  = irc.config.realName;
+  var chan      = irc.config.chan;
+
   irc.nick(nick, function() {
-    irc.act({action: 'USER ',
-             params: [user, 8,  '*'],
-             longParam: realName}, function() {
+    irc.user(user, '8', realName, function() {
       var has_admin = false;
       for (var u in irc.users) {
-        if (irc.users[u].auth === 'admin') {
+        if (irc.is_admin(u)) {
           has_admin = true;
         }
       }
       if (!has_admin) {
-        irc.act({action: 'JOIN', params: ['#bot_needs_admin']}, function() {
-          irc.emitter.on('hello', getNewAdmin);
-        });
+        throw ("An admin must be configured in users.json!");
+      }
+      
+      if (chan instanceof Array) {
+        for (var i = 0, l = chan.length; i < l; i += 1) {
+          irc.join(chan[i]);
+        }
+      } else {
+        irc.join(chan);
       }
     });
   });
@@ -172,6 +185,33 @@ irc.emitter.on('enable', function(act) {
     }
   } else {
     irc.privmsg(act.channel, 'ERROR: not authorized');
+  }
+});
+
+irc.emitter.on('set_auth', function (act) {
+  if (typeof user === 'undefined')
+    irc.users[nick] = {};
+
+  var user  = irc.users[nick];
+  var nick  = act.params[0];
+  var level = act.params[1];
+  if (act.params.length < 2) {
+    irc.privmsg(act.channel, 'ERROR: Insufficient parameters');
+    return (1);
+  }
+  if (level !== 'user' && level !== 'owner' && level !== 'admin') {
+    irc.privmsg(act.channel, 'ERROR: Invalid parameter: ' + level)
+    return (1);
+  }
+  if (irc.is_owner(act.nick)) {
+    if (nick === act.nick) {
+      irc.privmsg(act.channel, 'ERROR: Cannot change your own permissions!');
+    } else {
+      user.auth = level;
+      irc.privmsg(act.channel, nick + ' made to ' + level);
+    }
+  } else {
+    irc.privmsg(act.channel, 'ERROR: Insufficient privileges');
   }
 });
 
