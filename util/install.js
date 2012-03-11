e1e9fba3277ccc23b33d9fc8e340fdd90865a53e
Valter Strods <thatvalterguy@gmail.com>
2012-03-11 13:41:00 +0100

    small formatting improvements

--- util/install.js
+++ util/install.js
@@ -1,5 +1,5 @@
-var fs    = require('fs'),
-    path  = require('path');
+var fs    = require('fs');
+var path  = require('path');
 
 var isGlobal = process.env.npm_config_global;
 var configPath = path.resolve(process.env.HOME, '.ircnode');
@@ -14,7 +14,7 @@ if (!exists) {
 }
 
 var review_required = false;
-;[configFile, userFile].forEach(function (file) {
+[configFile, userFile].forEach(function (file) {
   var exists = path.existsSync(file);
   if (!exists) {
     var sample_file = './' + path.basename(file) + '.sample';
@@ -26,12 +26,12 @@ var review_required = false;
 if (review_required)
   console.log('Please review the configuration files in ' + configPath);
 
-
-;['sample', 'exec'].forEach(function (plugin) {
+['sample', 'exec'].forEach(function (plugin) {
   var pluginPath = path.resolve(pluginDir, plugin + '.js');
   var exists = path.existsSync(pluginPath);
   if (!exists) {
     var file = path.resolve('plugins', path.basename(pluginPath));
     fs.writeFileSync(pluginPath, fs.readFileSync(file));
   }
-})
+});
+
